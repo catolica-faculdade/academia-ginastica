fafe0f52fb11ded396318d7e343ac565980df39d
Levirroh <johann.gr14@gmail.com>
2025-10-27 10:04:21 -0300

    correção atributos

--- AcademiaGinastica/Classes/Usuario/Usuario.cs
+++ AcademiaGinastica/Classes/Usuario/Usuario.cs
@@ -8,17 +8,16 @@ public class Usuario
     private string telefone;
     private Tela tela;
 
-    public Usuario(string nome, string senha, string tipoUsuario)
+    public Usuario(string nome, string senha)
     {
-        this.nome = nome;
+        this.nomeCompleto = nome;
         this.senha = senha;
-        this.tipoUsuario = tipoUsuario;
         this.tela = new Tela(46, 12, 15, 12); 
     }
 
     static public Usuario Logar(string nome, string senha)
     {
-        Usuario usuario = new Usuario(nome, senha, "cliente");
+        Usuario usuario = new Usuario(nome, senha);
         return usuario;
     }
 
