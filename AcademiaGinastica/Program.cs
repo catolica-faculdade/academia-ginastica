b32c8cbe0ccf794c05153bc136e7f6091b87381f
Levirroh <johann.gr14@gmail.com>
2025-10-28 11:16:18 -0300

    fix: mensagem de erro ao entrar

--- AcademiaGinastica/Program.cs
+++ AcademiaGinastica/Program.cs
@@ -8,17 +8,18 @@ opcoes.Add("0 - Sair        ");
 while (true)
 {
     tela.PrepararTela("Academia de Ginástica");
-    opcao = tela.MostrarMenu(opcoes, 2, 2);
+    opcao = tela.MostrarMenu(opcoes, 35, 12);
 
     switch (opcao)
     {
+        case "0":
+            break;
         case "1":
             tela.Login();
             break;
-        case "0":
-            break;
         default:
-            tela.MostrarMensagem(10, 10, "Escolha inválida, digite 1 (Entrar) ou 2 (Cadastrar-se)");
+            tela.MostrarMensagem(15, 20, "Escolha inválida, pressione qualquer tecla para tentar novamente.");
+            Console.ReadKey();
             break;
     }
     if (opcao == "0")
