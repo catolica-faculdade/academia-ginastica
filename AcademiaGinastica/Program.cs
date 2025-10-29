a97f30c80a11f9021acf64c9663e1e17e30dc35a
Levirroh <johann.gr14@gmail.com>
2025-10-29 13:40:13 -0300

    fix: ajustando navegação do sistema

--- AcademiaGinastica/Program.cs
+++ AcademiaGinastica/Program.cs
@@ -1,33 +1,11 @@
-﻿using System.Reflection.Metadata;
-
-Tela tela = new Tela(90,25);
+﻿Tela tela = new Tela(90, 25);
+UsuarioController usuarioController = new UsuarioController();
 
 string opcao;
 List<string> opcoes = new List<string>();
 opcoes.Add("1 - Entrar      ");
 opcoes.Add("0 - Sair        ");
 
-#if WINDOWS
-        try
-        {
-            int larguraDesejada = 180;
-            int alturaDesejada = 50;
-
-            // Garante que o tamanho pedido não ultrapasse o máximo
-            int larguraMax = Console.LargestWindowWidth;
-            int alturaMax = Console.LargestWindowHeight;
-
-            int larguraFinal = Math.Min(larguraDesejada, larguraMax);
-            int alturaFinal = Math.Min(alturaDesejada, alturaMax);
-
-            Console.SetBufferSize(larguraFinal, alturaFinal);
-            Console.SetWindowSize(larguraFinal, alturaFinal);
-        }
-        catch
-        {
-            // Ignora se a plataforma não suportar
-        }
-#endif
 tela.ApagarArea(0, 0, 119, 49);
 while (true)
 {
@@ -62,10 +40,68 @@ while (true)
         case "0":
             break;
         case "1":
-            tela.Login();
+            bool entrou = tela.Login();
+
+            if (entrou)
+            {
+                bool sair = false;
+                while (!sair)
+                {
+                    opcao = tela.Home();
+                    switch (opcao)
+                    {
+                        case "1":
+                            string opcaoCadastro = tela.CadastrarUsuario();
+                            if (int.Parse(opcaoCadastro) >= 0 && int.Parse(opcaoCadastro) <= 6)
+                            {
+                                switch (opcaoCadastro)
+                                {
+                                    case "0":
+                                        break;
+                                    case "1":
+                                        tela.PrepararTela("CADASTRO DE USUARIO");
+                                        tela.MontarMoldura(2, 2, 60, 15);
+                                        tela.MostrarMensagem(4, 2, "Dados do novo usuario");
+                                        usuarioController.Cadastrar(5, 4, opcaoCadastro);
+                                        break;
+                                }
+                            }
+                            break;
+                        case "2":
+                            tela.CadastrarAula();
+                            break;
+                        case "3":
+                            tela.VerificarAgendas();
+                            break;
+                        case "4":
+                            string opcaoClientes = tela.VerificarClientes();
+                            if (int.Parse(opcaoClientes) >= 0 && int.Parse(opcaoClientes) <= 1)
+                            {
+                                if (opcaoClientes != "0")
+                                {
+                                    tela.PrepararTela("CADASTRO DE USUARIO");
+                                    tela.MontarMoldura(2, 2, 15, usuarioController.usuarios.Count * 2 + 4);
+                                    usuarioController.ListarUsuarios();
+                                    Console.ReadKey();
+                                }
+                            }
+                            break;
+                        case "5":
+                            tela.VerificarFuncionarios();
+                            break;
+                        case "6":
+                            tela.VerificarFuncionarios();
+                            break;
+                    }
+                    if (opcao == "0")
+                    {
+                        sair = true;
+                    }
+                }
+            }
             break;
         default:
-            tela.MostrarMensagem(15, 20, "Escolha inválida, pressione qualquer tecla para tentar novamente.");
+            tela.MostrarMensagem(15, 25, "[Escolha inválida, pressione qualquer tecla para tentar novamente]");
             Console.ReadKey();
             break;
     }
