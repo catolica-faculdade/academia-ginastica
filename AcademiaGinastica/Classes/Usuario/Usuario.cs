19426613352e735bd4715357068b08c01869e270
Levirroh <johann.gr14@gmail.com>
2025-10-29 13:42:41 -0300

    fix: ajustando funções

--- AcademiaGinastica/Classes/Usuario/Usuario.cs
+++ AcademiaGinastica/Classes/Usuario/Usuario.cs
@@ -1,42 +1,28 @@
+using System.Reflection.Metadata;
 public class Usuario
 {
-    private string nomeCompleto;
-    private string CPF;
-    private string email;
-    private string senha;
-    private string telefone;
-    private Tela? tela;
+    public string nomeCompleto;
+    public string CPF;
+    public string email;
+    public string senha;
+    public string telefone;
+    public string enderecoCompleto;
+    public Tela tela;
 
-    public Usuario(string nomeCompleto, string CPF, string email, string senha, string telefone)
+    public Usuario(string nomeCompleto, string cpf, string email, string senha, string telefone, string enderecoCompleto)
     {
         this.nomeCompleto = nomeCompleto;
+        this.CPF = cpf;
+        this.email = email;
         this.senha = senha;
+        this.telefone = telefone;
+        this.enderecoCompleto = enderecoCompleto;
         this.tela = new Tela(46, 12, 15, 12);
     }
+
     public Usuario()
     {
-        this.nomeCompleto = "";
-        this.senha = "";
     }
 
-    static public Usuario Logar(string nome, string senha)
-    {
-        Usuario usuario = new Usuario("", "", "", "", "");
-        return usuario;
-    }
-
-
-    static public string[] Cadastrar(string tipoCargo)
-    {
-
-        string nomeCompleto = Tela.Perguntar(2, 2, "Digite o nome completo do usuario");
-        string CPF = Tela.Perguntar(2, 2, "Digite o CPF do usuario");
-        string email = Tela.Perguntar(2, 2, "Digite o email do usuario");
-        string senha = Tela.PerguntarSenha(2, 2, "Digite a senha do usuario");
-
-        string[] usuario = [nomeCompleto, CPF, email, senha];
-
-        return usuario;
-    }
 
 }
