11666907426c0adfcb3a5a70d0fe17e6eb9e3b67
Levirroh <johann.gr14@gmail.com>
2025-10-31 11:58:01 -0300

    feat: constructor da aula

--- AcademiaGinastica/Classes/Aula/Aula.cs
+++ AcademiaGinastica/Classes/Aula/Aula.cs
@@ -1,16 +1,22 @@
 public class Aula
 {
-    private int id;
-    private string nome;
-    private Modalidade modalidade;
-    private Funcionario instrutor;
+    public string nome;
+    public Modalidade modalidade;
+    public Funcionario instrutor;
     public DateTime horarioInicio;
     public DateTime horarioFim;
     public List<Cliente> clientes;
-    private int lotacao;
+    public int lotacao;
 
-    public Aula(string modalidade, DateTime horario, List<Cliente> clientes, int lotacao)
+    public Aula(string nome, Modalidade modalidade, Funcionario instrutor, DateTime horarioInicio, DateTime horarioFim, List<Cliente> clientes, int lotacao)
     {
-        Console.WriteLine("teste Aula");
+        this.nome = nome;
+        this.modalidade = modalidade;
+        this.instrutor = instrutor;
+        this.horarioInicio = horarioInicio;
+        this.horarioFim = horarioFim;
+        this.clientes = clientes ?? new List<Cliente>(); 
+        this.lotacao = lotacao;
+
     }
 }
